568bac106a7cc03e000057f2755ed54c3be85149
unknown <alifali@code.edu.az>
2022-02-14 21:45:22 +0400

    task_added

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homework!");
+           
+            int[] arr = { 2, 4, 6, 8, 10, 12 };
+
+            Console.WriteLine(Array.LastIndexOf(arr, 12));
+            int num = Array.Find(arr, x => x == 6);
+            Console.WriteLine(num);
+            int[] num2 = Array.FindAll(arr, x => x == 8);
+            Console.WriteLine(num2);
+            {
+                Console.WriteLine(num2);
+            }
+
+
+            bool result = Array.Exists(arr, x => x == 2);
+            if (result) ;
+
+            else
+            {
+                Console.WriteLine("Bele bir eded yoxdur");
+            }
+
+
+        }
+    }
+}
